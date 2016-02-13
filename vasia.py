785732b27c05c9740bbd47a8233a23f48398c6be
oJieg <oleg151-13@mail.ru>
2016-02-14 00:07:15 +0300

    modified:   vasia.py

--- vasia.py
+++ vasia.py
@@ -1,11 +1,10 @@
-#z=int(input())
+z=int(input())
 
 def searth(n):
     ls=[]
     for i in range(2,n):
         if n%i==0:
             ls.append(i)
- #   print(ls)
     return ls
-#searth(z)
+print(searth(z))
 
