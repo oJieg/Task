cd759cef8c0a25c4c002b6a83fc91aab509b44cb
Kemiroff <workdfons@mail.ru>
2016-02-14 19:15:53 +0300

    Hello

--- zadanie3.py
+++ zadanie3.py
@@ -3,8 +3,10 @@ if a % 4 == 0:
  if a % 100 == 0:
    if a % 400 == 0:
     print("god visokosnui")
+   else:
+    print("god nevisokosnui")
  else:
   print("god visokosnui")
 else:
- print("god ne visokosnui") 
+ print("god nevisokosnui") 
  
